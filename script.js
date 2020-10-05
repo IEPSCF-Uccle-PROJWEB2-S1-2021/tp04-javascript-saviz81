cc48e6e77281b9815e7df3ffcac7d48090346d90
Saviz yahyazadeh <yasa2103@student.iepscf-uccle.be>
2020-10-05 10:05:39 +0200

    Tp04

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ max=a;
   }
   return max;
 }
-let x=function reduce(numbers,fn){
-  if(numbers.lenght<2){
+
+function reduce(numbers,fn){
+  if(numbers.length<2){
     return null;
     }
     else{
@@ -31,7 +32,7 @@ let x=function reduce(numbers,fn){
    return result;
   }
 }
-let y=function filter(numbers,fn){
+function filter(numbers,fn){
  let x=[];
  let j=0
   for (let i=0;i<numbers.length;i++){
@@ -44,10 +45,9 @@ let y=function filter(numbers,fn){
      return x;
 }
 let myTestNumbers=[5, 8, 13, 21, 34, 610, 55, 3, 89, 144, 233, 377];
-console.log(x(myTestNumbers,minimum));
-console.log(x(myTestNumbers,maximum));
-;
-console.log(y(myTestNumbers,a => { if(a>=30){return true;}}));
+console.log(reduce(myTestNumbers,minimum));
+console.log(reduce(myTestNumbers,maximum));
+console.log(filter(myTestNumbers,a => { if(a>=30){return true;}}));
 
 
 
